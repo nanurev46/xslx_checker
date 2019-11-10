b2134394e6552bb2b8548fe96f6c9b767a65664f
nanurev <kamikami11@yandex.ru>
2019-11-10 15:58:09 +0300

    first commit

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1199, 270);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_RowHeaderMouseClick);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(12, 317);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(1199, 270);
+            this.dataGridView2.TabIndex = 1;
+            this.dataGridView2.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView2_RowHeaderMouseClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 288);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Открыть";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 596);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Открыть";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 293);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(93, 601);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 644);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(551, 168);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 628);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Лог";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Location = new System.Drawing.Point(849, 288);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(362, 26);
+            this.panel1.TabIndex = 8;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(3, 3);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(149, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "сверху кейс/ снизу НСИ";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(167, 3);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(149, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "сверху НСИ/ снизу кейс";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1136, 591);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Сверка";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1222, 834);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        public DataTable openTable()
+        {
+            DataTable result = new DataTable();
+
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "Excel Files|*.xls;*.xlsx;*.xlsm";
+            openFile.InitialDirectory = "C:\\POS\\EORM\\17 release";
+            if (openFile.ShowDialog() == DialogResult.Cancel)
+                return result;
+
+
+            // получаем выбранный файл
+            string filename = openFile.FileName;
+            label1.Text = filename;
+
+            String name = "Sheet1";
+            String constr = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" +
+                            openFile.FileName +
+                            ";Extended Properties='Excel 12.0 XML;HDR=YES;';";
+            OleDbConnection con = new OleDbConnection(constr);
+            OleDbCommand oconn = new OleDbCommand("Select * From [" + name + "$]", con);
+            con.Open();
+
+            OleDbDataAdapter sda = new OleDbDataAdapter(oconn);
+            sda.Fill(result);
+
+            return result;
+        }
+
+        public static void findRecWithHands(DataGridView dsSource, DataGridView dgToFind)
+        {
+            int colCount = dsSource.ColumnCount;
+            int rowCount = dgToFind.RowCount - 1;//- строка с шапкой
+
+            if (checkColCount(dsSource, dgToFind))
+            {
+                return;
+            }
+
+            bool flag = true;
+            List<int> listNum = new List<int>();
+            for (int i = 0; i < rowCount; i++)
+            {
+                flag = true;
+                for (int j = 0; j < colCount; j++)
+                {
+                    if (!dsSource.SelectedRows[0].Cells[j].Value.ToString().Equals(dgToFind.Rows[i].Cells[j].Value.ToString()))
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+
+                if (flag)
+                {
+                    listNum.Add(i);
+                }
+
+            }
+            if (listNum.Count == 1)
+            {
+                dgToFind.ClearSelection();
+                dgToFind.Rows[listNum[0]].Selected = true;
+                dgToFind.CurrentCell = dgToFind.SelectedRows[0].Cells[0];
+            }
+
+            if (listNum.Count > 1)
+            {
+                String message = "Количество совпадений: " + listNum.Count + ". /r/n;" +
+                                "Номера строк: ";
+                foreach (int l in listNum)
+                {
+                    message = String.Concat(message + l + ", ");
+                }
+                MessageBox.Show("Количество столбцов в гридах не совпадает");
+            }
+
+
+            //логирование
+
+        }
+
+        public void findRecsAuto (DataGridView dgSource, DataGridView dgToFind)
+        {
+            int colCount = dgSource.ColumnCount;
+            int rowCountToFind = dgToFind.RowCount - 1;//- строка с шапкой
+            int rowCountSource = dgSource.RowCount - 1;//- строка с шапкой
+
+            if (checkColCount(dgSource, dgToFind))
+            {
+                return;
+            }
+
+            List<int> zeroCoincidence = new List<int>();//список ненайденных строк
+            List<int> oneCoincidence = new List<int>();//список найденных строк (1 совпадение)
+            List<int> manyCoincidence = new List<int>();//список найденных строк (больше 1 совпадения)
+
+            for (int i = 0; i < rowCountSource; i++)
+            {
+                switch (findRec(dgSource.Rows[i], dgToFind))
+                {
+                    case 0:
+                        zeroCoincidence.Add(i);
+                        break;
+                    case 1:
+                        oneCoincidence.Add(i);
+                        break;
+                    default:
+                        manyCoincidence.Add(i);
+                        break;
+                }
+            }
+
+            //Логи
+            richTextBox1.Text = "Дата проверки: " + DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss") + Environment.NewLine + Environment.NewLine +
+                                "Ненайденные строки (" + zeroCoincidence.Count() + "): ";
+            foreach (int z in zeroCoincidence)
+            {
+                richTextBox1.Text = String.Concat(richTextBox1.Text, z, " ");
+            }
+
+            richTextBox1.Text = String.Concat(richTextBox1.Text, Environment.NewLine, "Найденные строки (" + oneCoincidence.Count() + "): ");
+            foreach (int o in oneCoincidence)
+            {
+                richTextBox1.Text = String.Concat(richTextBox1.Text, o, " ");
+            }
+
+            richTextBox1.Text = String.Concat(richTextBox1.Text, Environment.NewLine, "Строки найдены боль 1 раза (" + manyCoincidence.Count() + "): ");
+            foreach (int m in manyCoincidence)
+            {
+                richTextBox1.Text = String.Concat(richTextBox1.Text, m, " ");
+            }
+
+            richTextBox1.Text = String.Concat(richTextBox1.Text, Environment.NewLine, "=========================", Environment.NewLine);
+
+
+        }
+
+        //Считаем кол-во вхождений искомой строки в НСИ
+        public static int findRec(DataGridViewRow row, DataGridView dgToFind)
+        {
+            int colCount = dgToFind.ColumnCount;
+            int rowCount = dgToFind.RowCount - 1;
+            /*
+            List<int> zeroCoincidence = new List<int>();//список ненайденных строк
+            List<int> oneCoincidence = new List<int>();//список найденных строк (1 совпадение)
+            List<int> manyCoincidence = new List<int>();//список найденных строк (больше 1 совпадения)
+            List<List<int>> result = new List<List<int>>();
+            */
+            int result = 0;
+
+            bool flag = false;
+            int recNum = 0; //для Логов
+            for (int i = 0; i < rowCount; i++)//по строкам НСИ
+            {
+                flag = false;
+                //counter = 0;
+                for (int j = 0; j < colCount; j++)//по ячейкам
+                {
+                    if (!row.Cells[j].Value.ToString().Equals(dgToFind.Rows[i].Cells[j].Value.ToString()))//если ячейки не совпали, переходим к следующей строке
+                    {
+                        break;
+                    }
+                    if (j == colCount - 1)//если все ячейки совпали
+                    {
+                        flag = true;
+                    }
+                }
+
+                if (flag)
+                {
+                    result++;
+                }
+
+            }
+
+            return result;
+        }
+
+        public static bool checkColCount(DataGridView dsSource, DataGridView dgToFind)
+        {
+            int colCount = dsSource.ColumnCount;
+            int rowCount = dgToFind.RowCount - 1;//- строка с шапкой
+
+            if (colCount != dgToFind.ColumnCount)
+            {
+                MessageBox.Show("Количество столбцов в гридах не совпадает");
+                return true;
+            }
+            return false;
+        }
+
+            private void button1_Click(object sender, EventArgs e)
+        {
+
+            dataGridView1.DataSource = openTable();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataGridView2.DataSource = openTable();
+
+        }
+
+        private void dataGridView1_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            findRecWithHands(dataGridView1, dataGridView2);
+/*
+            int colCount = dataGridView1.ColumnCount;
+            int rowCount = dataGridView2.RowCount-1;
+
+            if (colCount != dataGridView2.ColumnCount){
+                MessageBox.Show("Количество столбцов в гридах не совпадает");
+                return;
+            }
+
+                        bool flag = true;
+                        List<int> listNum = new List<int>();
+                        for (int i = 0; i < rowCount; i++)
+                        {
+                            flag = true;
+                            for (int j = 0; j < colCount; j++)
+                            {
+                                if (!dataGridView1.SelectedRows[0].Cells[j].Value.ToString().Equals(dataGridView2.Rows[i].Cells[j].Value.ToString())){
+                                    flag = false;
+                                    break;
+                                }
+                            }
+
+                            if (flag)
+                            {
+                                listNum.Add(i);
+                            }
+
+                        }
+                        if (listNum.Count == 1)
+                        {
+                            dataGridView2.ClearSelection();
+                            dataGridView2.Rows[listNum[0]].Selected = true;
+                            dataGridView2.CurrentCell = dataGridView2.SelectedRows[0].Cells[0];
+                        }
+
+                        if (listNum.Count > 1)
+                        {
+                            String message = "Количество совпадений: " + listNum.Count + ". /r/n;" +
+                                            "Номера строк: ";
+                            foreach (int l in listNum){
+                                message = String.Concat(message + l + ", ");
+                            }        
+                            MessageBox.Show("Количество столбцов в гридах не совпадает");
+                        }
+                        */
+        }
+
+        private void dataGridView2_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            findRecWithHands(dataGridView2, dataGridView1);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                findRecsAuto(dataGridView1, dataGridView2);
+            }
+            else
+            {
+                findRecsAuto(dataGridView2, dataGridView1);
+            }
+        }
+    }
+}
